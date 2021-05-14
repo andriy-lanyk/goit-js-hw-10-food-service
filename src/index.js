f6084c65d381d792b1af63a322146048553f6ced
andriy-lanyk <LAM1988@ukr.net>
2021-05-15 01:01:14 +0300

    Добавил переключение темы светлая/темная

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ import menuData from '../src/menu.json';
 
 const menuList = document.querySelector('.js-menu');
 const menuMarkup = createMenuMarkup(menuData);
+const buttonThemeSwitch = document.querySelector('#theme-switch-toggle');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const body = document.querySelector('body');
 
 menuList.insertAdjacentHTML('beforeend', menuMarkup);
 
 function createMenuMarkup(menuData) {
     return menuListTpl(menuData);
+};
+
+buttonThemeSwitch.addEventListener('change', changeMenuTheme);
+
+function changeMenuTheme() {
+    if (buttonThemeSwitch.checked) {
+        body.classList.add(Theme.DARK);
+        body.classList.remove(Theme.LIGHT);
+        localStorage.setItem('theme', 'dark');
+    } else {
+        body.classList.add(Theme.LIGHT);
+        body.classList.remove(Theme.DARK);
+        localStorage.setItem('theme', 'light');
+    };
+}
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+    buttonThemeSwitch.checked = true;
 }
 
